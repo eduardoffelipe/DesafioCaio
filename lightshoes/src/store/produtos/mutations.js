f4f9c5817a19e606d29c676d3db31a542803a73f
Eduardo Toledo <eduardoftoledo70@gmail.com.br>
2020-04-26 22:03:02 -0300

    adicionando trycatch nas mutations

--- lightshoes/src/store/produtos/mutations.js
+++ lightshoes/src/store/produtos/mutations.js
@@ -1,41 +1,51 @@
 import * as TYPES from "./types";
 export const mutations = {
   [TYPES.PUSH_PRODUCT_TO_CART]: (state, item) => {
-    if (state.cartItems.length != 0) {
-      const productIndex = state.cartItems.findIndex(
-        (p) => p.item.id == item.id
-      );
-      console.log("aqui 1");
-      if (productIndex != -1) {
-        state.cartItems[productIndex].quantidade += 1;
-        return;
-      }
+    try {
+      if (state.cartItems.length != 0) {
+        const productIndex = state.cartItems.findIndex(
+          (p) => p.item.id == item.id
+        );
+        if (productIndex != -1) {
+          state.cartItems[productIndex].quantidade += 1;
+          return;
+        }
 
-      state.cartItems.push({
-        item,
-        quantidade: 1,
-      });
-    } else {
-      console.log("aqui");
-      state.cartItems.push({
-        item,
-        quantidade: 1,
-      });
+        state.cartItems.push({
+          item,
+          quantidade: 1,
+        });
+      } else {
+        state.cartItems.push({
+          item,
+          quantidade: 1,
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
   },
 
   [TYPES.REMOVE_ITEM_CART_BY_ID]: (state, item) => {
-    const productIndex = state.cartItems.findIndex(
-      (product) => product.id == item.id
-    );
-    state.cartItems.splice(productIndex, 1);
+    try {
+      const productIndex = state.cartItems.findIndex(
+        (product) => product.id == item.id
+      );
+      state.cartItems.splice(productIndex, 1);
+    } catch (error) {
+      console.log(error);
+    }
   },
 
   [TYPES.SUBSTRACT_CART_BY_ID]: (state, { id }) => {
-    const productIndex = state.cartItems.findIndex(
-      (product) => product.id == id
-    );
-    if (state.cartItems[productIndex].quantidade <= 1) return;
-    state.cartItems[productIndex].quantidade -= 1;
+    try {
+      const productIndex = state.cartItems.findIndex(
+        (product) => product.id == id
+      );
+      if (state.cartItems[productIndex].quantidade <= 1) return;
+      state.cartItems[productIndex].quantidade -= 1;
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
